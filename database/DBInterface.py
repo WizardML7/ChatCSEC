3705c4a97c3e7d3a258ff5109061b752f7adeffa
Brian McNulty <brianmcnulty321@gmail.com>
2024-03-26 19:38:20 -0400

    updated interface to include createcollection

--- database/DBInterface.py
+++ database/DBInterface.py
@@ -1,6 +1,11 @@
 from abc import ABC, abstractmethod
 
 class iDB(ABC):
+
+    @abstractmethod
+    def createCollection(self, collectionName: str, size: int):
+        pass
+
     @abstractmethod
     def saveToDB(self, texts: dict, collectionName: str):
         pass

36e7fd62dd36cbdcf41787143524e3b073d3a0f9
Brian McNulty <brianmcnulty321@gmail.com>
2024-04-01 23:16:13 -0400

    Updated signature of createMbedding, created docstring

--- embed/embedInterface.py
+++ embed/embedInterface.py
@@ -1,7 +1,22 @@
 from abc import ABC, abstractmethod
 
 class iEmbed(ABC):
+    """
+    An interface used for all embedding models to support dependency injection, inversion and
+    polymorphism throughout the application.
+    """
     @staticmethod
     @abstractmethod
-    def createEmbedding(content: str, maxChunkSize: int, chunkOverlap: int, delimiter: str) -> dict:
+    def createEmbedding(content: str, maxChunkSize: int, chunkOverlap: int, delimiter: list) -> dict:
+        """
+        Takes in a string, chunks the string and embeds the chunks.
+        Args:
+            content (str): The string to be embedded.
+            maxChunkSize (int): The max size chunks should be.
+            chunkOverlap (int): The overlap between chunks.
+            delimiter (list): A list of delimiters that the splitter should chunk on.
+
+        Returns:
+            dict: a dictionary of all split chunks as keys and corresponding embeddings as values.
+        """
         pass

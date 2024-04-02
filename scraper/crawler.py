b16c0d11c0cc1275d10632f8dee7e70cdb1017d9
Brian McNulty <brianmcnulty321@gmail.com>
2024-04-01 21:30:56 -0400

    added crawl docstring

--- scraper/crawler.py
+++ scraper/crawler.py
@@ -64,6 +64,38 @@ class Crawler(iCrawler):
     def crawl(url: str, maxDepth: int,baseDirectory: list[str] = None, cores: int = 2,
               outputDirectory: str = os.path.dirname(os.path.realpath(__file__)),
               urlRegexString: str=None, contentRegexString: str=None, matchSkip: bool=False) -> set:
+        '''
+        Method to start crawling webpoages and downloading related content.
+
+        Notes:
+            It is important to use theContentRegexString argument in order to properly sanitize irrelevant information
+            such as website navbars, headers and footers.  This content can be detrimental to the results of the
+            embedding and retrieval in the RAG prompts, as well as increasing the token burden on API calls.
+
+        Args:
+            maxDepth (int): The max depth that the crawler should follow links to
+            baseDirectories (list): A list of URLs that specify the allowed url directories that the crawler will pull
+            webpages from.  If this is set to None, all URLs will be accepted.
+            cores (int): The amount of CPU cores for the crawler to use
+            outputDirectory (str): A path pointing to the output directory for the files downloaded and processed
+            urlRegexString (str): A string representing a regex to match URLs with.  If set to None, all URLs will be
+            accepted for download
+            contentRegexString (str): A regex used to extract content out of webpages.  It must follow the syntax of
+            (?:.LOOKABEHIND)(?P<content>.*)(?:LOOKBEHIND). If set to None, all content will be ingested
+            matchSkip (bool): If set to True, any content that does not match the contentRegexString will not be
+            downloaded. If contentRegexString is set to None, this variable is not used.
+
+        Returns:
+            Set: A set of links found by the crawler.
+
+        Examples:
+            >>> Crawler.crawl("https://openai.com/customer-stories", 1,
+            >>>     cores=4, baseDirectory=["https://openai.com/customer-stories"],
+            >>>     outputDirectory=os.path.dirname(os.path.realpath(__file__)) + "/test/",
+            >>>     urlRegexString=r'.*customer-stories.*a.*',
+            >>>     contentRegexString=r'(?:.*Customer stories)(?P<content>.*)(?:ResearchOverviewIndexGP.*)',
+            >>>     matchSkip=True)
+        '''
         if urlRegexString:
             urlRegex = re.compile(urlRegexString)
 

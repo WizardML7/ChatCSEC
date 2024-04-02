c70acf8fb26f95087691cb77a4f7853872dd423a
Brian McNulty <brianmcnulty321@gmail.com>
2024-04-01 23:22:37 -0400

    Updated embedprepper with docstrings

--- embed/embedPrepper.py
+++ embed/embedPrepper.py
@@ -3,8 +3,24 @@ import tiktoken
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 class EmbedPrepper:
+    """
+    A collection of methods used to prepare text to be embedded by embedding models
+    """
     @staticmethod
-    def chunkTextBySize(content: str, maxChunkSize: int=800, chunkOverlap: int=100, delimiter: list=["\n\n", "\n", " ", ""]) -> list:
+    def chunkTextBySize(content: str, maxChunkSize: int=800,
+                        chunkOverlap: int=100, delimiter: list=["\n\n", "\n", " ", ""]) -> list:
+        """
+        Chunks text by the given delimiters aiming for a maximum size per chunk.
+
+        Args:
+            content (str): The string to turn into chunks
+            maxChunkSize (int): The maximum size for chunks to be
+            chunkOverlap (int): The amount of text for the chunks to overlap with
+            delimiter (list): A list of delimiters for the splitter to split on
+
+        Returns:
+            list: List of chunks from the text
+        """
         text_splitter = RecursiveCharacterTextSplitter(
             separators=delimiter,
             chunk_size=maxChunkSize,
@@ -14,8 +30,17 @@ class EmbedPrepper:
         # print(content)
         chunks = text_splitter.split_text(content)
         return chunks
+
     @staticmethod
     def removeExtraWhitespace(content: str) -> str:
+        """
+        A method to remove the extra whitespace from a string
+        Args:
+            content (str): a string to rmeove whitespace from
+
+        Returns:
+            str: The content string stripped of newlines
+        """
         content = content.replace("\n", " ")\
                     .replace("\r", " ")\
                     .replace("\xa0", " ")

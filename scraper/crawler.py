867a3ad8edd4fdba15a490be1f097028725d8841
Brian McNulty <brianmcnulty321@gmail.com>
2024-04-12 01:14:42 -0400

    Added User Agent Header

--- scraper/crawler.py
+++ scraper/crawler.py
@@ -48,7 +48,9 @@ class Crawler(iCrawler):
             "text/html": HTMLHandler
         }
 
-        content = requests.get(url)
+        #Fake chrome user agent
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
+        content = requests.get(url, headers=headers)
         contentType = content.headers["content-type"].split(";")[0]
 
         if recordUrl:
